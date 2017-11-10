17dd784ba2e1d7584fdbb4ba7c2decfb405d8ded
pebriq <pebriq@pebriq-server>
2017-11-10 15:47:34 +0900

    17/11/10

---
+++ HadoopPrj/src/hadoopfile/Program.java
@@ -0,0 +1,49 @@
+package hadoopfile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		
+		Configuration conf = new Configuration();
+		FileSystem hdfs = FileSystem.get(conf);
+		
+		Path filePath = new Path("/input/data.txt");
+		
+		if(!hdfs.exists(filePath)) {
+			System.out.println("입력오류 : " + filePath + "가 존재하지 않습니다.");
+			System.exit(2);
+		}
+		
+		//FileInputStream fis = new FileInputStream("res/data.txt");
+		FSDataInputStream fis = hdfs.open(filePath);
+		
+		Scanner fscan = new Scanner(fis);
+		
+		int total = 0;
+		
+		while(fscan.hasNext()) {
+		
+			String line = fscan.nextLine();
+			String[] tokens = line.split("\t");
+			float lateTime = Float.parseFloat(tokens[1]);
+			total += lateTime;
+			
+		}
+		
+		System.out.println(total);
+		
+		fis.close();
+		
+	}
+		
+}
